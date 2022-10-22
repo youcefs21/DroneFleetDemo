79bd5fd0a2694b262fd795878910aec6ca48b926
youcef <youcefs2121@gmail.com>
2022-10-22 12:40:40 -0400

    user endpoint

--- src/pages/api/v0/user/[cuid].ts
+++ src/pages/api/v0/user/[cuid].ts
@@ -1,8 +1,23 @@
-import { NextApiRequest, NextApiResponse } from "next";
+import type { NextApiRequest, NextApiResponse } from "next";
+import { prisma } from "../../../../server/db/client";
+import { z } from "zod";
+
+const stringVerify = z.string()
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
 
-  res.status(200).json({ query: req.query });
+  const cuid = stringVerify.safeParse(req.query.cuid);
+
+  if (!cuid.success) {
+    res.status(400).json({ errors: cuid.error.issues });
+    return;
+  }
+
+  const user = await prisma.user.findUnique({
+    where: {id: cuid.data },
+  })
+
+  res.status(200).json({ user: user })
 }
 
 
