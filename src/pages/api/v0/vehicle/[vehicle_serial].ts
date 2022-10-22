1dfc85bf4b365cbdd8aa338aa1db0f2fe4560feb
youcef <youcefs2121@gmail.com>
2022-10-22 12:37:03 -0400

    vehicle endpoint

--- src/pages/api/v0/vehicle/[vehicle_serial].ts
+++ src/pages/api/v0/vehicle/[vehicle_serial].ts
@@ -1,8 +1,23 @@
-import { NextApiRequest, NextApiResponse } from "next";
+import type { NextApiRequest, NextApiResponse } from "next";
+import { prisma } from "../../../../server/db/client";
+import { z } from "zod";
+
+const stringVerify = z.string()
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
 
-  res.status(200).json({ query: req.query });
+  const vehicle_serial  = stringVerify.safeParse(req.query.vehicle_serial);
+
+  if (!vehicle_serial.success) {
+    res.status(400).json({ errors: vehicle_serial.error.issues });
+    return;
+  }
+
+  const vehicle = await prisma.vehicle.findUnique({
+    where: { vehicle_serial: vehicle_serial.data },
+  })
+
+  res.status(200).json({ vehicle: vehicle })
 }
 
 
