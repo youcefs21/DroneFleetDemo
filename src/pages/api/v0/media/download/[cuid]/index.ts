28cb8c218882ebc4aa98d3605fc9bc360f592432
youcef <youcefs2121@gmail.com>
2022-10-22 17:17:43 -0400

    download url endpoint

--- src/pages/api/v0/media/download/[cuid]/index.ts
+++ src/pages/api/v0/media/download/[cuid]/index.ts
@@ -1,8 +1,28 @@
-import { NextApiRequest, NextApiResponse } from "next";
+import type { NextApiRequest, NextApiResponse } from "next";
+import { prisma } from "../../../../../../server/db/client";
+import { z } from "zod";
+
+const reqSchema = z.object({
+  cuid: z.string(),
+})
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
 
-  res.status(200).json({ query: req.query });
+  const query = reqSchema.safeParse(req.query);
+
+  if (!query.success) {
+    res.status(400).json({ errors: query.error.issues });
+    return;
+  }
+
+  const media = await prisma.media.findUnique({
+    where: {id: query.data.cuid },
+    select: {
+      download_url: true,
+    }
+  })
+
+  res.status(200).json(media)
 }
 
 
