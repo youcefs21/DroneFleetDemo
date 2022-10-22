d2e3a9e5af15f150a503f1a89820fe785e64cad3
youcef <youcefs2121@gmail.com>
2022-10-22 15:22:16 -0400

    scan endpoint

--- src/pages/api/v0/scan/[cuid].ts
+++ src/pages/api/v0/scan/[cuid].ts
@@ -1,8 +1,35 @@
-import { NextApiRequest, NextApiResponse } from "next";
+import type { NextApiRequest, NextApiResponse } from "next";
+import { prisma } from "../../../../server/db/client";
+import { z } from "zod";
+
+const reqSchema = z.object({
+  cuid: z.string(),
+  files_page_number: z.number().int().default(1),
+  files_per_page: z.number().int().default(25),
+  pre_signed_download_url: z.boolean().optional(),
+})
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
 
-  res.status(200).json({ query: req.query });
+  const query = reqSchema.safeParse(req.query);
+
+  if (!query.success) {
+    res.status(400).json({ errors: query.error.issues });
+    return;
+  }
+
+  const scan = await prisma.scan.findUnique({
+    where: {scan_id: query.data.cuid },
+    include: {
+      flight: true,
+      files: {
+        skip: (query.data.files_page_number - 1) * query.data.files_per_page,
+        take: query.data.files_per_page,
+      }
+    }
+  })
+
+  res.status(200).json({ scan: scan })
 }
 
 
