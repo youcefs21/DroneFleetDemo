3694211561360e01e3344038405b0fd478d77e7e
youcef <youcefs2121@gmail.com>
2022-10-22 17:17:19 -0400

    flight endpoint

--- src/pages/api/v0/flight/[flight_id]/index.ts
+++ src/pages/api/v0/flight/[flight_id]/index.ts
@@ -1,8 +1,25 @@
-import { NextApiRequest, NextApiResponse } from "next";
+import type { NextApiRequest, NextApiResponse } from "next";
+import { prisma } from "../../../../../server/db/client";
+import { z } from "zod";
+
+const reqSchema = z.object({
+  cuid: z.string(),
+})
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
 
-  res.status(200).json({ query: req.query });
+  const query = reqSchema.safeParse(req.query);
+
+  if (!query.success) {
+    res.status(400).json({ errors: query.error.issues });
+    return;
+  }
+
+  const flight = await prisma.flight.findUnique({
+    where: {id: query.data.cuid },
+  })
+
+  res.status(200).json({flight: flight})
 }
 
 
