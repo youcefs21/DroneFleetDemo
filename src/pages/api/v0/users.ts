a4a0b7962209f230335129eb459c3da4c971fd97
youcef <youcefs2121@gmail.com>
2022-10-22 09:26:26 -0400

    users API endpoint

--- src/pages/api/v0/users.ts
+++ src/pages/api/v0/users.ts
@@ -1,8 +1,48 @@
-import { NextApiRequest, NextApiResponse } from "next";
+import type { NextApiRequest, NextApiResponse } from "next";
+import { prisma } from "../../../server/db/client";
+import { z } from "zod";
+
+
+const reqSchema = z.object({
+  first_name: z.string(),
+  last_name: z.string(),
+  email: z.string(),
+  organization_role: z.enum(["UNKNOWN", "MEMBER", "TESTER", "MODERATOR", "ADMIN"]),
+  page_number: z.number().int().default(1),
+  per_page: z.number().int().default(25),
+});
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  const query = reqSchema.safeParse(req.query);
 
-  res.status(200).json({ query: req.query });
+  if (!query.success) {
+    res.status(400).json({ errors: query.error.issues });
+    return;
+  }
+
+  const where = {
+    first_name: query.data.first_name,
+    last_name: query.data.last_name,
+    email: query.data.email,
+    organization_role: query.data.organization_role,
+  }
+
+  const users = await prisma.user.findMany({
+    where: where,
+    skip: (query.data.page_number - 1) * query.data.per_page,
+    take: query.data.per_page,
+  });
+
+  const total = await prisma.user.count({where: where});
+
+  res.status(200).json({
+    pagination: {
+      current_page: query.data.page_number,
+      max_per_page: query.data.per_page,
+      total_pages: Math.ceil(total / query.data.per_page),
+    },
+    users: users
+  });
 }
 
 
